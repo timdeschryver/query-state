bad987f62904f92928b17c7dc7c5fd1235fb54f6
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2022-09-24 17:42:35 +0000

    chore: run nx migrate

--- apps/example-app/jest.config.ts
+++ apps/example-app/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'query-state',
+/* eslint-disable */
+export default {
+  displayName: 'example-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/query-state',
+  coverageDirectory: '../../coverage/apps/example-app',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
--- libs/query-state/jest.config.ts
+++ libs/query-state/jest.config.ts
@@ -1,5 +1,6 @@
-module.exports = {
-  displayName: 'example-app',
+/* eslint-disable */
+export default {
+  displayName: 'query-state',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +9,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/example-app',
+  coverageDirectory: '../../coverage/libs/query-state',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
